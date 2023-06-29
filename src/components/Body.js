14e45aca1ab437edd06b47adc224807d72b23e09
ManishaLalage <manishalalage91@gmail.com>
2023-06-29 14:09:43 +0530

    all changes done...

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,13 +3,11 @@ import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
 const Body = () => {
-   
   return (
     <div className='flex'>
       <Sidebar/>
       <Outlet/>
     </div>
-  );
-}
+  );}
 
 export default Body;
